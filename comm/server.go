11b12c42996280772098ca6a0fe853e4f147b8a6
Payson1019 <599591947@qq.com>
2024-12-10 14:22:57 +0800

    Bug fixes

--- comm/server.go
+++ comm/server.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"net/rpc"
 	"os"
+	"strings"
 	"sync"
 
 	"tee-dao/logger"
@@ -57,20 +58,20 @@ func (srv *Server) Close() {
 func (srv *Server) ListenTLS() {
 	srv.logger.Info("Starting TLS server")
 
-	srv.logger.Debug("Loading server key pair",
+	srv.logger.With("func", "ListenTLS").Debug("Loading server key pair",
 		slog.String("cert", srv.cfg.Cert), slog.String("key", srv.cfg.Key))
 	serverCert, err := tls.LoadX509KeyPair(srv.cfg.Cert, srv.cfg.Key)
 	if err != nil {
-		srv.logger.Error("Failed to load server certificate", slog.String("err", err.Error()))
+		srv.logger.With("func", "ListenTLS").Error("Failed to load server certificate", slog.String("err", err.Error()))
 		return
 	}
 
 	caCertPool := x509.NewCertPool()
 	for _, peer := range srv.cfg.Peers {
-		srv.logger.Debug("Loading CA cert", slog.String("ca", peer.CACert))
+		srv.logger.With("func", "ListenTLS").Debug("Loading CA cert", slog.String("ca", peer.CACert))
 		caCert, err := os.ReadFile(peer.CACert)
 		if err != nil {
-			srv.logger.Error("Failed to read CA certificate", slog.String("err", err.Error()))
+			srv.logger.With("func", "ListenTLS").Error("Failed to read CA certificate", slog.String("err", err.Error()))
 			return
 		}
 		caCertPool.AppendCertsFromPEM(caCert)
@@ -82,12 +83,15 @@ func (srv *Server) ListenTLS() {
 		ClientAuth:   tls.RequireAndVerifyClientCert,
 	}
 
-	listener, err := tls.Listen("tcp", srv.cfg.Address, tlsConfig)
+	// Setup the server listen to ipd 0.0.0.0 with port in the config
+	serverAddress := "0.0.0.0:" + strings.Split(srv.cfg.Address, ":")[1]
+	listener, err := tls.Listen("tcp", serverAddress, tlsConfig)
 	if err != nil {
-		srv.logger.Error("Failed to start TLS server", slog.String("err", err.Error()))
+		srv.logger.With("func", "ListenTLS").Error("Failed to start TLS server", slog.String("err", err.Error()))
 		return
 	}
 
+	srv.logger.With("func", "ListenTLS").Debug("TLS server started", slog.String("address", listener.Addr().String()))
 	srv.listener = listener
 
 	// loop stops after srv.listener is explicitly closed by srv.Close()
@@ -101,7 +105,7 @@ func (srv *Server) ListenTLS() {
 			continue
 		}
 
-		srv.logger.Debug("Client connected", slog.String("from", conn.RemoteAddr().String()))
+		srv.logger.With("func", "ListenTLS").Debug("Client connected", slog.String("from", conn.RemoteAddr().String()))
 
 		// routine to wait for client to disconnect and then close the connection
 		srv.wg.Add(1)
@@ -115,7 +119,7 @@ func (srv *Server) ListenTLS() {
 func (srv *Server) ListenRPC() {
 	srv.logger.Info("Starting RPC server")
 
-	srv.logger.With("func", "ListenRPC").Debug("Loading server key pair",
+	srv.logger.With("func", "ListenRPC").With("func", "ListenRPC").Debug("Loading server key pair",
 		slog.String("cert", srv.cfg.Cert), slog.String("key", srv.cfg.Key))
 	serverCert, err := tls.LoadX509KeyPair(srv.cfg.Cert, srv.cfg.Key)
 	if err != nil {
@@ -140,12 +144,15 @@ func (srv *Server) ListenRPC() {
 		ClientAuth:   tls.RequireAndVerifyClientCert,
 	}
 
-	rpcListener, err := tls.Listen("tcp", srv.cfg.RPCAddress, tlsConfig)
+	// Setup the server listen to ipd 0.0.0.0 with port in the config
+	serverAddress := "0.0.0.0:" + strings.Split(srv.cfg.RPCAddress, ":")[1]
+	rpcListener, err := tls.Listen("tcp", serverAddress, tlsConfig)
 	if err != nil {
 		srv.logger.With("func", "ListenRPC").Error("Failed to start RPC server", slog.String("err", err.Error()))
 		return
 	}
 
+	srv.logger.With("func", "ListenRPC").Debug("RPC server started", slog.String("address", rpcListener.Addr().String()))
 	srv.rpcListener = rpcListener
 
 	// loop stops after srv.rpcListener is explicitly closed by srv.Close()
